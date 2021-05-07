997af8b7370b46f61562ffbbb546b6122bdf14dc
Teresa Nowakowski <TeresaNowakowski2023@u.northwestern.edu>
2021-05-07 17:24:36 -0500

    adding tutorial06 files

--- tutorial05/font-switcher/js/index.js
+++ tutorial05/font-switcher/js/index.js
@@ -1,5 +1,12 @@
 const makeBigger = () => {
-   document.querySelector(".content").style.fontSize = "4em";
+   // document.querySelector(".content").style.fontSize = "4em";
+   // let OG = document.querySelector(".content").fontSize.value;
+   let OG = document.querySelector(".content").style.fontSize;
+   console.log(OG);
+   let newSize = Number(OG) * 2
+   // newSize = OG + 1;
+   console.log(newSize);
+   document.querySelector(".content").style.fontSize = newSize;
 };
 
 const makeSmaller = () => {
---
+++ tutorial06/index.js
@@ -0,0 +1,136 @@
+const tracks = [
+    {
+      "id": "3cr3oAP4bQFNjZBV7ElKaB",
+      "name": "I Know The End",
+      "artist": "Phoebe Bridgers",
+      "preview_url": "https://p.scdn.co/mp3-preview/2d438eeb5d734e12b09bdb66bcae81ad720eab5b?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b2733040ca980277cf1445934add"
+    },
+    {
+      "id": "3iji9D5iOwGSCz1bnkuAii",
+      "name": "Spinning (with Charli XCX & The 1975)",
+      "artist": "No Rome, Charli XCX & The 1975",
+      "preview_url": "https://p.scdn.co/mp3-preview/dd29f003c15326250c9d2ae3d70457411ecaf202?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b2734f5549dc1dd07183ee7941ba"
+    },
+    {
+      "id": "I Know A Place",
+      "name": "I Know A Place",
+      "artist": "MUNA",
+      "preview_url": "https://p.scdn.co/mp3-preview/22cd69ef9886bc7ead93898020e43413ca2234b0?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b273120baf06468dbd74a40582ca"
+    },
+    {
+      "id": "1BYZxKSf0aTxp8ZFoeyM3d",
+      "name": "we fell in love in october",
+      "artist": "girl in red",
+      "preview_url": "https://p.scdn.co/mp3-preview/5a6397654aa282c6bd99af417d58142caddcd3a9?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b273eb13fbc012815ea6e834e842"
+    },
+    {
+      "id": "1BwhFXqoIsePt21WyWIttb",
+      "name": "I Wanna Get Better",
+      "artist": "Bleachers",
+      "preview_url": "https://p.scdn.co/mp3-preview/8e0ade4b15192e27b888d21cf87ce1499bde3862?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b2734849278ce9876ebea7353d66"
+    },
+    {
+      "id": "5bwLs2TUxldWW6ZGOI9Zx6",
+      "name": "Crush",
+      "artist": "Ethel Cain",
+      "preview_url": "https://p.scdn.co/mp3-preview/6125980a698210c54b8a418a4930b8302259cb47?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b27303f810a82a20982f66ccd45a"
+    },
+    {
+      "id": "5Jo9dAhHEdBOsCbOPq0hti",
+      "name": "Fire Drills",
+      "artist": "Dessa",
+      "preview_url": "https://p.scdn.co/mp3-preview/baac7f9395b586b79a8799c35e5df62f21a71057?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b273660ba4e185634633d66ef26b"
+    },
+    {
+      "id": "7h8tRiEeBvte0H7n0h8Xg4",
+      "name": "Hardline",
+      "artist": "Julien Baker",
+      "preview_url": "https://p.scdn.co/mp3-preview/93d1207ed68b44473f9ce81600168bc5cce97431?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b2733086e90166d3c2ca266b7c67"
+    },
+    {
+      "id": "56lhDZNQ5J47aog6mGKeGk",
+      "name": "Thunder Road",
+      "artist": "Bruce Springsteen",
+      "preview_url": "https://p.scdn.co/mp3-preview/76a59582754a4169c339c679257763293fe791ba?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b273503143a281a3f30268dcd9f9"
+    },
+    {
+      "id": "2uwN3PDudlRkAKE3wA0OqJ",
+      "name": "Angeles",
+      "artist": "Elliott Smith",
+      "preview_url": "https://p.scdn.co/mp3-preview/9a01d7293a0e3bafa51b72829924904c22a0d6c2?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b27336e934bdc6f83dd9d022389d"
+    },
+    {
+      "id": "4ReJJcpW8HtVnqzhf6DwNV",
+      "name": "Close To You",
+      "artist": "Dayglow",
+      "preview_url": "https://p.scdn.co/mp3-preview/33ea3f3a056a28acdba806ea54a482e290c71bb3?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b273a28330e1b98a8d1c39e06389"
+    },
+    {
+      "id": "7znbs5T41Li5UBiVGhEACQ",
+      "name": "Summer Years",
+      "artist": "Death Cab for Cutie",
+      "preview_url": "https://p.scdn.co/mp3-preview/188a877f32f83527b792d58fec0993a70da0560f?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b27397bf91e5ecee3470d063614f"
+    },
+    {
+      "id": "0VaIsQoIQY10uxBrOyZ5cQ",
+      "name": "May I Have This Dance (Remix) [feat. Chance the Rapper]",
+      "artist": "Francis and the Lights, Chance the Rapper",
+      "preview_url": "https://p.scdn.co/mp3-preview/6fed5464d95633911da067965255aab0496edc99?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b273b12c66cbee07ff97c6fe2d2e"
+    },
+    {
+      "id": "5n8aqbNFFYJ5J1m3FaXl13",
+      "name": "Lovesick",
+      "artist": "Maude Latour",
+      "preview_url": "https://p.scdn.co/mp3-preview/0e696c5c44d1e3abc14f60e012186bafdcbf2215?cid=9697a3a271d24deea38f8b7fbfa0e13c",
+      "image_url": "https://i.scdn.co/image/ab67616d0000b273ae6b24d6b4d9b1dbf9d7de86"
+    }
+];
+
+
+// Part 1: 
+// This code adds a card for the 6th track in the list (above)
+// How would you use a loop so that a card is generated for every 
+// track in the list?
+
+var i;
+for(i=0; i < tracks.length; i++) {
+  const track = tracks[i];
+  const template = `
+      <div data-index="${i}" onclick="playSong(event);">
+          <img src="${track.image_url}" />
+          <h2>${track.name}</h2>
+          <h3>${track.artist}</h3>
+      </div>`;
+  document.querySelector('main').innerHTML += template;
+}
+
+
+// Part 2: 
+// Using the event object, detect the element that triggered the
+// click event (currentTarget) to figure out which song to load 
+// into the player next. When you've figured it out, set the 
+// #audio-source element's "src" attribute with the correct 
+// sound sample and then invoke the audio.load() and audio.play() 
+// logic.
+const playSong = (ev) => {
+  var index = Number(ev.currentTarget.dataset.index);
+  document.getElementById("audio-source").src = tracks[index].preview_url;
+  const audio = document.querySelector('audio');
+  audio.load();
+  audio.play();
+}
+
+
