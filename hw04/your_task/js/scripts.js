4474fae7511690a4c3b0ff9eafc11e3ab7fffa5a
Teresa Nowakowski <TeresaNowakowski2023@u.northwestern.edu>
2021-05-18 15:21:44 -0500

    updating hw04

--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -17,17 +17,17 @@ const search = (ev) => {
 }
 
 const getTracks = (term) => {
-    url = baseURL + "?type=track&q=" + term;
+    url = baseURL + "?type=track&q=" + term + "&limit=5";
     fetch(url)
         .then(response => response.json())
         .then(data => displayTracks(data));
 };
 
 const getAlbums = (term) => {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    url = baseURL + "?type=album&q=" + term;
+    fetch (url)
+        .then(response => response.json())
+        .then(data => displayAlbums(data));
 };
 
 const getArtist = (term) => {
@@ -40,31 +40,60 @@ const getArtist = (term) => {
 
 document.querySelector('#search').onkeyup = (ev) => {
     // Number 13 is the "Enter" key on the keyboard
-    console.log(ev.keyCode);
+    // console.log(ev.keyCode);
     if (ev.keyCode === 13) {
         ev.preventDefault();
         search();
     }
 };
 
+const displayAlbums = (foundAlbums) => {
+    if (foundAlbums[0] == null) {
+        document.querySelector('#albums').innerHTML = "no albums found that match your search";
+    } else {
+        document.querySelector('#albums').innerHTML = " ";
+        const lenAlbums = foundAlbums.length;
+        for (t=0; t<lenAlbums; t++) {
+            template = `<section class="album-card" id="${foundAlbums[t].id}">
+                            <div>
+                                <img src="${foundAlbums[t].image_url}">
+                                <h3>${foundAlbums[t].name}</h3>
+                                <div class="footer">
+                                    <a href="${foundAlbums[t].spotify_url}" target="_blank">
+                                    view on spotify
+                                    </a>
+                                </div>
+                            </div>
+                        </section>`;
+            document.querySelector('#albums').innerHTML += template;
+        }
+    }
+}
+
 const displayTracks = (foundTracks) => {
     if (foundTracks[0] == null) {
-        document.querySelector('#tracks').innerHTML = " ";
+        document.querySelector('#tracks').innerHTML = "no tracks found that match your search";
     } else {
-        document.querySelector('#tracks').innerHTML += template;
-        const lenTracks = foundTracks.length;
-        for (t=0; t < Math.min(5, lenTracks); t++){
-            template = `<section class="track-item preview" data-preview-track="${foundTracks[t].preview_url}">
-                            <img src="${foundTracks[t].album.image_url}">
+        document.querySelector('#tracks').innerHTML = " ";
+        for (const item of foundTracks) {
+            template = `<section class="track-item preview" data-preview-track="${item.preview_url}">
+                            <img src="${item.album.image_url}">
                             <i class="fas play-track fa-play" aria-hidden="true"></i>
                             <div class="label">
-                                <h3>${foundTracks[t].name}</h3>
-                                <p> ${foundTracks[t].artist.name} </p>
+                                <h3>${item.name}</h3>
+                                <p> ${item.artist.name} </p>
                             </div>
                         </section>`;
             document.querySelector('#tracks').innerHTML += template;
-        }
+            item.onclick = playTrack;
     }
+}
+}
+
+playTrack = (ev) => {
+    const queuedTrack = ev.currentTarget;
+    audioPlayer.setAudioFile(queuedTrack.preview_url);
+    audioPlayer.play();
 }
 
 const displayArtist = (art) => {
